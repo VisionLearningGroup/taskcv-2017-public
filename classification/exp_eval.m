7186ee79563e83f244271acef446b6a33735f06a
xcpeng <jackjack.peng@gmail.com>
2017-06-18 21:40:07 -0400

    update exp_eval

--- classification/exp_eval.m
+++ classification/exp_eval.m
@@ -1,14 +1,40 @@
-pred_label = textread('./result/pred.txt');
-gt_label = textread('./result/val_gt.txt');
+function result = exp_eval()
+
+% ---------------------------------------------------------
+% This file is part of VISDA-17 challenge code. 
+% This function is to calculate the mean accuarcy. 
+% Please modify the following paths accordingly when you
+% call this function:
+% 1. pred_file_path : path to your prediction text file
+% 2. gt_file_path   : path to the ground truth text file 
+% Contact: Xingchao Peng (xpeng@bu.edu)
+% ---------------------------------------------------------
+
+% ---------------- User Configuration----------------------
+pred_file_path = './result/prediction.txt';
+gt_path = './result/val_ground_truth.txt';
+% ------------------End of configuration--------------------
+
+
+pred_label = textread(pred_file_path);
+gt_label = textread(gt_path);
 category_num  = 12;
+category = {'aeroplane', 'bicycle', 'bus', 'car', 'horse', 'knife', ...
+    'motorcycle' , 'person',  'plant', 'skateboard',  'train', 'truck'};
 
 
-[C,index] = confusionmat( pred_label, gt_label);
-x_sum = sum(C);
-C = double(C);
-p=zeros(category_num,1);
+[Confmat,index] = confusionmat(pred_label, gt_label);
+x_sum = sum(Confmat);
+Confmat = double(Confmat);
+sub_accuracy=zeros(category_num,1);
+
+fprintf('Sub-accuracy:\n');
+fprintf('=================');
 for j =1:category_num
-	C(:,j) = C(:,j)/x_sum(j);
-	p(j) = C(j,j);
-	fprintf('%.1f &', p(j)*100);
+	Confmat(:,j) = Confmat(:,j)/x_sum(j);
+	sub_accuracy(j) = C(j,j);
+	fprintf('%s | %0.1f\n', category(j) ,sub_accuracy(j)*100);
 end
+
+fprintf('\n\n');
+fprintf('Mean accuracy: %.2f\n', sum(sub_accuracy)/length(sub_accuracy));
